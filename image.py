18ea447d872857c49951e4b14977eedc60ec3e68
przemo1232 <p.sobczak01@gmail.com>
2023-10-14 11:11:33 +0200

    add dither

--- image.py
+++ image.py
@@ -11,7 +11,7 @@ def mse(pixel: list, ref: list) -> float:
         err += (x-y)**2
     return err / 3
 
-def choose_tile(pixel: list) -> str:
+def choose_tile(pixel: list, width: int) -> str:
     """choosing the appropriate tile"""
     tile = ''
     err = 1e5
@@ -20,7 +20,26 @@ def choose_tile(pixel: list) -> str:
         if bound < err:
             err = bound
             tile = color
-    return tile
+    err = []
+    for old_color, new_color in zip(pixel, tile_to_color[tile]):
+        err.append(old_color - new_color)
+    return tile, err
+
+def multiply_list(mult: float, pixel: list):
+    for x in pixel:
+        x *= mult
+    return pixel
+
+def dither(i: int, jump: int, err: list, width: int, values: list):
+    """Floyd–Steinberg dithering"""
+    if (i // jump) % width != width - 1:
+        values[i + jump: i + jump + 3] += multiply_list(7/16, err)
+    try:
+        values[i + (width - 1) * jump: i + (width - 1) * jump + 3] += multiply_list(3/16, err)
+        values[i + width * jump: i + width * jump + 3] += multiply_list(5/16, err)
+        values[i + (width + 1) * jump: i + (width + 1) * jump + 3] += multiply_list(1/16, err)
+    except IndexError:
+        pass
 
 def make_string(in_filename: str, out_filename: str):
     """make string from png file"""
@@ -37,7 +56,9 @@ def make_string(in_filename: str, out_filename: str):
 
     tiles = []
     for i in range(0, len(values), jump):
-        tiles.append(choose_tile(values[i: i + jump]))
+        tile, err = choose_tile(values[i: i + 3], width)
+        tiles.append(tile)
+        dither(i, jump, err, width, values)
 
     # actually making the json
     output = {'blueprint': {'tiles': [], 'item': 'blueprint', 'version': 281479277707264}}
