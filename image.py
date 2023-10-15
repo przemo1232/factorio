e91caefde5933c4cf6d8e6fb8a10b6a5400410be
przemo1232 <79700515+przemo1232@users.noreply.github.com>
2023-10-15 19:56:56 +0200

    refactor

--- image.py
+++ image.py
@@ -4,42 +4,50 @@ import png
 import json
 from config import full_py as tile_to_color
 
-def mse(pixel: list, ref: list) -> float:
-    """error between tile and reference"""
-    err = 0
-    for x, y in zip(pixel, ref):
-        err += (x-y)**2
-    return err / 3
-
-def choose_tile(pixel: list, width: int) -> str:
-    """choosing the appropriate tile"""
+class Pixel:
+    values = []
+    pos = 0
+    x = 0
+    y = 0
     tile = ''
-    err = 1e5
-    for color in tile_to_color:
-        bound = mse(pixel, tile_to_color[color])
-        if bound < err:
-            err = bound
-            tile = color
-    err = []
-    for old_color, new_color in zip(pixel, tile_to_color[tile]):
-        err.append(old_color - new_color)
-    return tile, err
+    def __init__(self, colors):
+        self.values = colors[0:3]
 
-def multiply_list(mult: float, pixel: list):
-    for x in pixel:
-        x *= mult
-    return pixel
+    def mse(self, ref: list) -> float:
+        """error between tile and reference"""
+        err = 0
+        for x, y in zip(self.values, ref):
+            err += (x-y)**2
+        return err / 3
 
-def dither(i: int, jump: int, err: list, width: int, values: list):
-    """Floyd–Steinberg dithering"""
-    if (i // jump) % width != width - 1:
-        values[i + jump: i + jump + 3] += multiply_list(7/16, err)
-    try:
-        values[i + (width - 1) * jump: i + (width - 1) * jump + 3] += multiply_list(3/16, err)
-        values[i + width * jump: i + width * jump + 3] += multiply_list(5/16, err)
-        values[i + (width + 1) * jump: i + (width + 1) * jump + 3] += multiply_list(1/16, err)
-    except IndexError:
-        pass
+    def choose_tile(self):
+        """choosing the appropriate tile"""
+        err = 1e5
+        for color in tile_to_color:
+            bound = self.mse(tile_to_color[color])
+            if bound < err:
+                err = bound
+                self.tile = color
+        err = []
+        for old_color, new_color in zip(self.values, tile_to_color[self.tile]):
+            err.append((old_color - new_color) >> 4)
+        return err
+    
+    def add_values(self, values: list):
+        for i, err_value in enumerate(values):
+            self.values[i] += err_value
+
+def dither(pixel: Pixel, err: list, width: int, height: int, pixels: list[Pixel]):
+    """Floyd-Steinberg dithering"""
+    if pixel.x < width - 1:
+        print(f"old: {pixels[pixel.pos + 1].values}")
+        pixels[pixel.pos + 1].add_values([value * 7 for value in err])
+        if pixel.y < height - 1:
+            pixels[pixel.pos + width + 1].add_values(err)
+        print(f"new: {pixels[pixel.pos + 1].values}")
+    if pixel.y < height - 1:
+        pixels[pixel.pos + width - 1].add_values([value * 3 for value in err])
+        pixels[pixel.pos + width].add_values([value * 5 for value in err])
 
 def make_string(in_filename: str, out_filename: str):
     """make string from png file"""
@@ -52,13 +60,20 @@ def make_string(in_filename: str, out_filename: str):
         jump = 4
     image = image.read_flat()
     width = image[0]
+    height = image[1]
     values = image[2].tolist()
-
     tiles = []
+    pixels: list[Pixel] = []
+
     for i in range(0, len(values), jump):
-        tile, err = choose_tile(values[i: i + 3], width)
-        tiles.append(tile)
-        dither(i, jump, err, width, values)
+        pixels.append(Pixel(values[i: i + 3]))
+    for i, pixel in enumerate(pixels):
+        pixel.pos = i
+        pixel.x = i % width
+        pixel.y = i // width
+        err = pixel.choose_tile()
+        dither(pixel, err, width, height, pixels)
+        tiles.append(pixel.tile)
 
     # actually making the json
     output = {'blueprint': {'tiles': [], 'item': 'blueprint', 'version': 281479277707264}}
